22d3711ddf8e7fd84751c14d679839cde68d167a
120L020818 <120L020818@stu.hit.edu.cn>
2022-10-17 18:23:15 +0800

    添加Django ORM

---
+++ vuedata/models.py
@@ -0,0 +1,45 @@
+from django.db import models
+
+# Create your models here.
+
+class userTable(models.Model):
+    UserName = models.CharField(max_length=32)
+    Password = models.CharField(max_length=32)
+    Email = models.CharField(max_length=32)
+    Sex = models.CharField(max_length=1)
+    Birthday = models.DateField()
+    Phone = models.CharField(max_length=32)
+    Salt = models.CharField(max_length=32)
+
+class applyTable(models.Model):
+    RegistrationNumber = models.CharField(max_length=32)
+    SerialNumber = models.CharField(max_length=32)
+    Organization = models.CharField(max_length=32)
+    StartTime = models.DateField()
+    EndTime = models.DateField()
+    JuridicalPerson= models.CharField(max_length=32)
+    ChargePerson= models.CharField(max_length=32)
+    ChargePhone= models.CharField(max_length=32)
+    UserName= models.CharField(max_length=32)
+    PublicKey=models.CharField(max_length=32)
+
+class certTable(models.Model):
+    RegistrationNumber = models.CharField(max_length=32)
+    SerialNumber = models.CharField(max_length=32)
+    Organization = models.CharField(max_length=32)
+    StartTime = models.DateField()
+    EndTime = models.DateField()
+    JuridicalPerson= models.CharField(max_length=32)
+    ChargePerson= models.CharField(max_length=32)
+    ChargePhone= models.CharField(max_length=32)
+    UserName= models.CharField(max_length=32)
+    PublicKey=models.CharField(max_length=32)
+    CertPathName=models.CharField(max_length=32)
+
+class crlTable(models.Model):
+    SerialNumber=models.CharField(max_length=32)
+    Organization=models.CharField(max_length=32)
+    RevokeTime = models.DateField()
+
+
+
